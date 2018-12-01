fa14e6babbc5fd0a114d036c17bfef2d0b4461d5
wamilomarov <wamil95omarov@gmail.com>
2018-12-01 17:32:35 +0100

    Added forwarding headers

--- Billsplitter/Startup.cs
+++ Billsplitter/Startup.cs
@@ -8,6 +8,7 @@ using Billsplitter.Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -72,7 +73,12 @@ namespace Billsplitter
             {
                 app.UseHsts();
             }
-
+            
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+            
             app.UseAuthentication();
 
             app.UseHttpsRedirection();
